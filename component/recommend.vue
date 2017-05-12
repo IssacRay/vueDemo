28e291399b113697c1a6878a8b50e21ffb038d2b
IssacRay <935860019@qq.com>
2017-05-12 20:34:06 +0800

    增加relist组件

--- component/recommend.vue
+++ component/recommend.vue
@@ -1,13 +1,16 @@
 <template>
 	<div>
 		<p>推荐页</p>
+		<relist></relist>
 	</div>
 </template>
 
 <script>
+import relist from "relist.vue"
+
 	module.exports={
 		components:{
-			
+			relist,
 		}
 	}
 </script>
---
+++ component/relist.vue
@@ -0,0 +1,39 @@
+<template>
+	<div>
+		<nav class="relist">
+			<li>
+				<img src="" alt="" v-if="1">
+				<div>
+					<h3>这是标题</h3>
+					<p>这是内容</p>
+				</div>
+			</li>
+		</nav>
+	</div>
+</template>
+
+<script>
+	module.exports={
+		data:function(){
+
+		}
+	}
+</script>
+
+<style>
+	.relist{
+		width: 100%;
+		height: 68px;
+		padding: 2px;
+	}
+	.relist li img{
+		width: 68px;
+		height: 68px;
+		float:left;
+		background-color: #fc0;
+	} 
+	.relist li div{
+		float:left;
+		background-color: red;
+	}
+</style>
